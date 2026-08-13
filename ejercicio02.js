ae71b6965b9d40cd7b271bab36678e6a90d4d17c
fabiola2406065 <fabiola2406065@hybridge.education>
2026-08-12 19:10:00 -0600

    Update ejercicio02.js

--- ejercicio02.js
+++ ejercicio02.js
@@ -1,8 +1,10 @@
 // Ejercicio 2: Uso de Constantes en Funciones
 // Declara una función llamada "obtenerPI" que retorne el valor 3.1416
 
-//define tu función aquí
-
+function obtenerPI() {
+    const PI = 3.1416;
+    return PI;
+}
 
 // No modificar
 // Exporta la función para que pueda ser utilizada en otros archivos
