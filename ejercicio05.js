cdf57168984c872a27eca607652b6369b82ea2b0
fabiola2406065 <fabiola2406065@hybridge.education>
2026-08-12 19:14:59 -0600

    Update ejercicio05.js

--- ejercicio05.js
+++ ejercicio05.js
@@ -1,8 +1,9 @@
 // Ejercicio 5: Área de un Rectángulo
 // Declara una función llamada "calcularAreaRectangulo" que acepte dos parámetros, "ancho" y "alto", y retorne el área del rectángulo
 
-//escribe tu función aquí. Recuerda que el nombre exacto de la función es "calcularAreaRectangulo"
-
+function calcularAreaRectangulo(ancho, alto) {
+    return ancho * alto;
+}
 
 // Exporta la función para que pueda ser utilizada en las pruebas
 module.exports = {
